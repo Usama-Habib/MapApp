873867b285797204f33cfa3e0a7a173eae972c80
Muhammad Usama <usamahabib1231@outlook.com>
2024-02-24 01:23:29 +0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import {
+  MapContainer,
+  TileLayer,
+  Polygon
+} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { statesData } from './data';
+
+const center = [40.63463151377654, -97.89969605983609];
+
+function App() {
+  return (
+    <MapContainer
+      center={center}
+      zoom={5}
+      style={{ width: '100vw', height: '100vh' }}
+    >
+      <TileLayer
+        url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=wL8o1TmUNcPDncQl6y35"
+        attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+      />
+      {
+        statesData.features.map((state) => {
+          const coordinates = state.geometry.coordinates[0].map((item) => [item[1], item[0]]);
+
+          return (<Polygon
+            pathOptions={{
+              fillColor: '#FD8D3C',
+              fillOpacity: 0.7,
+              weight: 2,
+              opacity: 1,
+              dashArray: 3,
+              color: 'white'
+            }}
+            positions={coordinates}
+            eventHandlers={{
+              mouseover: (e) => {
+                const layer = e.target;
+                layer.setStyle({
+                  dashArray: "",
+                  fillColor: "#BD0026",
+                  fillOpacity: 0.7,
+                  weight: 2,
+                  opacity: 1,
+                  color: "white",
+                })
+              },
+              mouseout: (e) => {
+                const layer = e.target;
+                layer.setStyle({
+                  fillOpacity: 0.7,
+                  weight: 2,
+                  dashArray: "3",
+                  color: 'white',
+                  fillColor: '#FD8D3C'
+                });
+              },
+              click: (e) => {
+
+              }
+            }}
+          />)
+        })
+      }
+    </MapContainer>
+  );
+}
+
+export default App;
